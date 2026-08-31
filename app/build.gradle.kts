f141b6f261ba25af6ca62d894af3aa03d3c0ef50
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-08-31 22:24:32 +0300

    release: prepare 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.github.stanleyll0yd.impulse"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
