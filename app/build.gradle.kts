5189ad8874e86343ae7179bf006e4a7c92c2b29e
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-07 16:00:12 +0200

    release: bump IMPULSE to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.sl.impulse"
         minSdk = 26
         targetSdk = 37
-        versionCode = 15
-        versionName = "1.0.3"
+        versionCode = 16
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
